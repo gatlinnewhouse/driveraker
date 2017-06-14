84fd46296ab43e21b6f3f61e4aedecfaa4bc708c
gatlinnewhouse <gatlinnewhouse@gmail.com>
2017-06-14 22:47:38 +0000

    Docx works better than html to sync and convert to markdown

--- sync.sh
+++ sync.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Pull/Sync the files
-drive pull -desktop-links=false -export html $REMOTE_DIR
+drive pull -desktop-links=false -export docx $REMOTE_DIR
 
 # Convert files to Markdown
 pandoc --atx-headers --smart --normalize --email-obfuscation=references --mathjax -t markdown_strict -o $FILE.md $FILE.html
