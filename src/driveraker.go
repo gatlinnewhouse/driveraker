8f0782fe2db7707e08f55bec9febfed6e509744a
gatlinnewhouse <gatlinnewhouse@gmail.com>
2017-08-22 17:19:25 -0700

    Fixed #9. Driveraker adds quotation marks around image paths when moving them to new directories now.

--- src/driveraker.go
+++ src/driveraker.go
@@ -651,10 +651,10 @@ func readMarkdownWriteHugoHeaders(markdownFilePath string, docxFilePath string, 
 	var imagenames []string
 	imagenames, i = regexLineOfMarkdown(markdownfile.Contents, `(\w+.png)`, `<img src=`, i)
 	imagename := imagenames[1]
-	coverImagePathBefore := path.Dir(path.Dir(docxFilePath)) + "/" + imagename
-	//fmt.Println("image path before: " + "\"" + coverImagePathBefore + "\"")
-	coverImagePathAfter := hugoDirectory + "static/images/" + imagename
-	//fmt.Println("image path after: " + "\"" + coverImagePathAfter + "\"")
+	coverImagePathBefore := "\"" + path.Dir(path.Dir(docxFilePath)) + "/" + imagename "\""
+	//fmt.Println("image path before: " + coverImagePathBefore)
+	coverImagePathAfter := "\"" + hugoDirectory + "static/images/" + imagename + "\""
+	//fmt.Println("image path after: " + coverImagePathAfter)
 	copyCoverImage := exec.Command("/bin/cp", coverImagePathBefore, coverImagePathAfter)
 	copyCoverImage.Dir = "/"
 	fmt.Println("Moving cover image image to hugo directory...")
@@ -736,8 +736,8 @@ func readMarkdownWriteHugoHeaders(markdownFilePath string, docxFilePath string, 
 			rewriteimageline.Add(1)
 			re2 := regexp.MustCompile(`(\w+.png)`)
 			inlineImage := re2.FindAllString(markdownfile.Contents[j], -1)
-			inlineImagePathBefore := path.Dir(path.Dir(docxFilePath)) + "/" + inlineImage[1]
-			inlineImagePathAfter := hugoDirectory + "static/images/" + inlineImage[1]
+			inlineImagePathBefore := "\"" + path.Dir(path.Dir(docxFilePath)) + "/" + inlineImage[1] + "\""
+			inlineImagePathAfter := "\"" + hugoDirectory + "static/images/" + inlineImage[1] + "\""
 			copyImage := exec.Command("/bin/cp", inlineImagePathBefore, inlineImagePathAfter)
 			copyImage.Dir = "/"
 			fmt.Println("Moving inline image to hugo directory...")
