015b2b96834b906ffaf34ccab31b72b715ad8cd2
Gatlin Newhouse <gatlinnewhouse@users.noreply.github.com>
2019-03-04 13:04:57 -0800

    Create shortcut.sh
    A quick little script for cleaning up between tests.
    
    Project is paused or abandoned until further notice.

---
+++ shortcut.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+trash ~/drivetemp/.gd/drivedb
+trash ~/.config/driveraker/.driverakerdb
+trash ~/drivetemp/The_Quest/
+trash ~/hugo-test-site/content/articles/*
+trash ~/hugo-test-site/static/images/*
